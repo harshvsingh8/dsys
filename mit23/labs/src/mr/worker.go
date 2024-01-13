41befad61540e13bdd17931f78d71a9c3869f9c5
Harsh Vardhan Singh <harshvsingh8@gmail.com>
2024-01-14 04:47:48 +0530

    ts

--- mit23/labs/src/mr/worker.go
+++ mit23/labs/src/mr/worker.go
@@ -1,10 +1,11 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,11 +25,11 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
-func Worker(mapf func(string, string) []KeyValue,
+func Worker(
+	mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
